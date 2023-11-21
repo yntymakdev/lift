5ff5a9e7357bf157e0f094fecca400e04f3d51d8
yntymakdev <kubanychevyntymak21@gmail.com>
2023-11-21 11:09:51 +0600

    initial

---
+++ script.js
@@ -0,0 +1,151 @@
+let btn16 = document.querySelector(".btn16");
+let btn15 = document.querySelector(".btn15");
+let btn14 = document.querySelector(".btn14");
+let btn13 = document.querySelector(".btn13");
+let btn12 = document.querySelector(".btn12");
+let btn11 = document.querySelector(".btn11");
+let btn10 = document.querySelector(".btn10");
+let btn9 = document.querySelector(".btn9");
+let btn8 = document.querySelector(".btn8");
+let btn7 = document.querySelector(".btn7");
+let btn6 = document.querySelector(".btn6");
+let btn5 = document.querySelector(".btn5");
+let btn4 = document.querySelector(".btn4");
+let btn3 = document.querySelector(".btn3");
+let btn2 = document.querySelector(".btn2");
+let btn1 = document.querySelector(".btn1");
+//!
+let block1 = document.querySelector(".block1");
+let block2 = document.querySelector(".block2");
+let block3 = document.querySelector(".block3");
+let block4 = document.querySelector(".block4");
+let block5 = document.querySelector(".block5");
+let block6 = document.querySelector(".block6");
+let block7 = document.querySelector(".block7");
+let block8 = document.querySelector(".block8");
+let block9 = document.querySelector(".block9");
+let block10 = document.querySelector(".block10");
+let block11 = document.querySelector(".block11");
+let block12 = document.querySelector(".block12");
+let block13 = document.querySelector(".block13");
+let block14 = document.querySelector(".block14");
+let block15 = document.querySelector(".block15");
+let block16 = document.querySelector(".block16");
+
+// let block3 = document.querySelector(".block3");
+
+let w = document.querySelector(".w img");
+
+btn16.addEventListener("click", () => {
+  w.style.transform = "translateY(-620px)";
+  block1.style.background = "green";
+  block2.style.background = "coral";
+  block3.style.background = "coral";
+});
+
+btn15.addEventListener("click", () => {
+  w.style.transform = "translateY(-570px)";
+  //   block3.style.background = "green";
+  block2.style.background = "green";
+  block1.style.background = "coral";
+});
+btn14.addEventListener("click", () => {
+  w.style.transform = "translateY(-530px)";
+  block3.style.background = "green";
+
+  block1.style.background = "coral";
+  block2.style.background = "coral";
+});
+// block2.style.background = "green";
+
+btn13.addEventListener("click", () => {
+  w.style.transform = "translateY(-480px)";
+  block1.style.background = "coral";
+  block2.style.background = "coral";
+  block3.style.background = "coral";
+});
+
+btn12.addEventListener("click", () => {
+  w.style.transform = "translateY(-440px)";
+  block1.style.background = "coral";
+  block2.style.background = "coral";
+  block3.style.background = "coral";
+});
+
+btn11.addEventListener("click", () => {
+  w.style.transform = "translateY(-420px)";
+  block1.style.background = "coral";
+  block3.style.background = "coral";
+
+  block2.style.background = "coral";
+});
+
+btn10.addEventListener("click", () => {
+  w.style.transform = "translateY(-360px)";
+  block1.style.background = "coral";
+  block2.style.background = "coral";
+  block3.style.background = "coral";
+});
+
+btn9.addEventListener("click", () => {
+  w.style.transform = "translateY(-320px)";
+  block1.style.background = "coral";
+  block3.style.background = "coral";
+
+  block2.style.background = "coral";
+});
+
+btn8.addEventListener("click", () => {
+  w.style.transform = "translateY(-280px)";
+  block1.style.background = "coral";
+  block2.style.background = "coral";
+  block3.style.background = "coral";
+});
+
+btn7.addEventListener("click", () => {
+  w.style.transform = "translateY(-240px)";
+  block1.style.background = "coral";
+  block2.style.background = "coral";
+  block3.style.background = "coral";
+});
+
+btn6.addEventListener("click", () => {
+  w.style.transform = "translateY(-200px)";
+  block1.style.background = "coral";
+  block2.style.background = "coral";
+  block3.style.background = "coral";
+});
+
+btn5.addEventListener("click", () => {
+  w.style.transform = "translateY(-160px)";
+  block1.style.background = "coral";
+  block3.style.background = "coral";
+});
+
+btn4.addEventListener("click", () => {
+  w.style.transform = "translateY(-120px)";
+  block1.style.background = "coral";
+  block2.style.background = "coral";
+  block3.style.background = "coral";
+});
+
+btn3.addEventListener("click", () => {
+  w.style.transform = "translateY(-80px)";
+  block1.style.background = "coral";
+  block2.style.background = "coral";
+  block3.style.background = "coral";
+});
+
+btn2.addEventListener("click", () => {
+  w.style.transform = "translateY(-40px)";
+  block1.style.background = "coral";
+  block2.style.background = "coral";
+  block3.style.background = "coral";
+});
+
+btn1.addEventListener("click", () => {
+  w.style.transform = "translateY(-0px)";
+  block1.style.background = "coral";
+  block2.style.background = "coral";
+  block3.style.background = "coral";
+});
